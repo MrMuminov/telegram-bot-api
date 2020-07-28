dc4de1cf41c5ed229ef87f0c966f64cc47104dd3
Bahriddin Mo'minov <darkshadeuz@gmail.com>
2020-07-28 09:22:20 +0500

    Add new methods * setWebhook * deleteWebhook * sendPhoto

--- Bot.php
+++ Bot.php
@@ -11,6 +11,32 @@ namespace mrmuminov\tgbot;
  */
 class Bot extends BaseBot
 {
+
+    /**
+     * @param $url string
+     * @param null $certificate new CURLFile("pathOfFile/file.crt")
+     * @param null $max_connections
+     * @param bool $allowed_updates
+     * @return mixed
+     */
+    public static function setWebhook($url, $certificate = null, $max_connections = null, $allowed_updates = false)
+    {
+        return self::request([
+            'url' => $url,
+            'certificate' => $certificate,
+            'max_connections' => $max_connections,
+            'allowed_updates' => $allowed_updates,
+        ], "setWebhook");
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function deleteWebhook()
+    {
+        return self::request([], "deleteWebhook");
+    }
+
     /**
      * @param string|integer $chat_id
      * @param string $text
@@ -20,9 +46,9 @@ class Bot extends BaseBot
      * @param integer $reply_to_message_id
      * @param keyboard\Keyboard $reply_markup
      */
-    public static function sendMessage($chat_id, $text, $parse_mode = "", $disable_web_page_preview = false, $disable_notification = false, $reply_to_message_id = null, keyboard\Keyboard $reply_markup = null)
+    public static function sendMessage($chat_id, $text, $parse_mode = null, $disable_web_page_preview = false, $disable_notification = false, $reply_to_message_id = null, keyboard\Keyboard $reply_markup = null)
     {
-        self::request([
+        return self::request([
             'chat_id' => $chat_id,
             'text' => $text,
             'parse_mode' => $parse_mode,
@@ -41,11 +67,37 @@ class Bot extends BaseBot
      */
     public static function forwardMessage($chat_id, $from_chat_id, $message_id, $disable_notification = false)
     {
-        self::request([
+        return self::request([
             'chat_id' => $chat_id,
             'from_chat_id' => $from_chat_id,
             'message_id' => $message_id,
             'disable_notification' => $disable_notification,
         ], "forwardMessage");
     }
+
+
+    /**
+     * @param $chat_id
+     * @param $photo
+     * @param null $caption
+     * @param null $parse_mode
+     * @param bool $disable_notification
+     * @param null $reply_to_message_id
+     * @param keyboard\Keyboard|null $reply_markup
+     * @return mixed
+     */
+    public static function sendPhoto($chat_id, $photo, $caption = null, $parse_mode = null, $disable_notification = false, $reply_to_message_id = null, keyboard\Keyboard $reply_markup = null)
+    {
+        return self::request([
+            'chat_id' => $chat_id,
+            'photo' => $photo,
+            'caption' => $caption,
+            'parse_mode' => $parse_mode,
+            'disable_notification' => $disable_notification,
+            'reply_to_message_id' => $reply_to_message_id,
+            'reply_markup' => $reply_markup,
+        ], "sendPhoto");
+    }
+
+
 }
